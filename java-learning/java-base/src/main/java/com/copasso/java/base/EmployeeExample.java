995c8d817bafe25a8a93c2377536c64af5ee9a7d
zas023 <375027533@qq.com>
2018-02-12 16:16:13 +0800

    Java学习笔记-对象和类

---
+++ java-learning/java-base/src/main/java/com/copasso/java/base/EmployeeExample.java
@@ -0,0 +1,51 @@
+package com.copasso.java.base;
+
+class Employee{
+    String name;
+    int age;
+    String designation;
+    double salary;
+    // Employee 类的构造器
+    public Employee(String name){
+        this.name = name;
+    }
+    // 设置age的值
+    public void empAge(int empAge){
+        age =  empAge;
+    }
+    /* 设置designation的值*/
+    public void empDesignation(String empDesig){
+        designation = empDesig;
+    }
+    /* 设置salary的值*/
+    public void empSalary(double empSalary){
+        salary = empSalary;
+    }
+    /* 打印信息 */
+    public void printEmployee(){
+        System.out.println("Name:"+ name );
+        System.out.println("Age:" + age );
+        System.out.println("Designation:" + designation );
+        System.out.println("Salary:" + salary);
+    }
+}
+
+public class EmployeeExample {
+
+    public static void main(String args[]){
+        /* 使用构造器创建两个对象 */
+        Employee empOne = new Employee("James Smith");
+        Employee empTwo = new Employee("Mary Anne");
+
+        // 调用这两个对象的成员方法
+        empOne.empAge(26);
+        empOne.empDesignation("Senior Software Engineer");
+        empOne.empSalary(1000);
+        empOne.printEmployee();
+
+        empTwo.empAge(21);
+        empTwo.empDesignation("Software Engineer");
+        empTwo.empSalary(500);
+        empTwo.printEmployee();
+    }
+}
