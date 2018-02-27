b994713f28316a072d14cd1658bbe89869c1b79b
zas023 <375027533@qq.com>
2018-02-27 21:53:25 +0800

    Day1

---
+++ java-learning/code/exception/TestException.java
@@ -0,0 +1,14 @@
+/*
+*测试异常
+*/
+public class TestException{
+	public static void main(String[] args) {
+		int i=1/0;  //ArithmeticException
+	}
+}
+
+/*异常：是Java提供的用于程序中处理错误的一种机制
+  
+  Java采用面向对象的方式来处理异常，其过程：
+  1. 抛出异常： 在执行一个方法时，如果
+*/
