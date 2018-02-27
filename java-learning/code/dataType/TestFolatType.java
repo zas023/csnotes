07e37af7b2df7183177983e5ed2f8467ad6b120c
zas023 <375027533@qq.com>
2018-02-27 20:20:10 +0800

    Day1

--- java-learning/code/dataType/TestFolatType.java
+++ java-learning/code/dataType/TestFolatType.java
@@ -9,6 +9,11 @@ public class TestFolatType{
 
 		//float f = 3.14;  //报错
 		float f = 3.14F;
+
+		float f1=0.1f;
+		double d1=1.0/10;
+		//避免在比较中使用浮点数
+		System.out.println(f1==d1);  //输出false，因为浮点数有误差
 	}
 }
 
@@ -18,6 +23,9 @@ public class TestFolatType{
 2. double类型精确度是float的两倍，又称双精度浮点数，绝大多数程序采用double
 
 */
+
+//float的范围为-2^128 ~ +2^128，也即-3.40E+38 ~ +3.40E+38；
+//double的范围为-2^1024 ~ +2^1024，也即-1.79E+308 ~ +1.79E+308。
 
 //浮点数默认为double类型，要将其变为float,需要在后面加"f"或"F"
 
