256d359dd6663e09e2ac72340870926dd42fdd41
zas023 <375027533@qq.com>
2018-03-03 17:13:35 +0800

    Day4

--- java-learning/code/oop/polymorphism/TestPolymorphism.java
+++ java-learning/code/oop/polymorphism/TestPolymorphism.java
@@ -26,6 +26,14 @@ public class TestPolymorphism{
 		
 		testCat(new Cat());
 
+		Animal a=new Cat();
+		//编译时，a声明的是Animal类型，就会去Animal类中查找相应的成员
+		//       如果没有改成员，则编译无法通过
+		//执行过程中，a指向的是一个Cat类的实例对象，则a的类型就是Cat
+
+		//a.play();//如法通过编译
+		((Cat)a).play();
+
 		testAnimal(new Cat());  //父类的引用指向一个子类对象
 		testAnimal(new Dog());
 	}
@@ -44,6 +52,10 @@ class Cat extends Animal{
 
 	public void voice(){
 		System.out.println("喵喵喵");
+	}
+
+	public void play(){
+		System.out.println("上树");
 	}
 }
 
@@ -73,6 +85,15 @@ class Pig extends Animal{
    - 执行时类型（运行时，具体是哪个子类就是那个子类）
          - 由实际对象的类型决定
 
+    //编译时，变量声明的什么类型就是什么类型，执行时，创建的什么类型对象就是什么类型
+
 3. 多态的三个必要条件
    - 要有继承、要有方法重写、父类引用指向子类对象
+*/
+
+/*
+Animal a=new Cat()
+//编译时，a声明的是Animal类型，就会去Animal类中查找相应的成员
+//       如果没有改成员，则编译无法通过
+//执行过程中，a指向的是一个Cat类的实例对象，则a的类型就是Cat
 */
