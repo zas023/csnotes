9b8c6e3192384520e5588c3962bb72563d5a56e7
zas023 <375027533@qq.com>
2018-05-24 21:55:44 +0800

    add java project

---
+++ java/projects/Square/src/main/java/com/copasso/tetris/model/State.java
@@ -0,0 +1,20 @@
+package com.copasso.tetris.model;
+
+/**
+ * 方块状态
+ */
+public enum State {
+	/**
+	 * 运动的方块
+	 */
+	ACTIVE,
+	/**
+	 * 静止的方块
+	 */
+	STOPED,
+	/**
+	 * 空方块
+	 */
+	EMPTY
+}
+
---
+++ java/projects/Square/src/main/java/com/copasso/tetris/util/Constant.java
@@ -0,0 +1,11 @@
+package com.copasso.tetris.util;
+
+public class Constant {
+
+    // 每个方块的边长
+    public static final int BLOCK_SIZE = 30;
+    // 游戏空间行数
+    public static final int NUM_ROW = 20;
+    // 游戏空间列数
+    public static final int NUM_COL = 15;
+}
