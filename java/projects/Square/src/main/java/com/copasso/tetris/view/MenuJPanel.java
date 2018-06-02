a004b6e84485eccef7c5dde5cdc2a8fbb9f6f6b8
zas023 <375027533@qq.com>
2018-06-02 17:44:15 +0800

    Java Project

--- java/projects/Square/src/main/java/com/copasso/tetris/view/MenuJPanel.java
+++ java/projects/Square/src/main/java/com/copasso/tetris/view/MenuJPanel.java
@@ -19,18 +19,21 @@ public class MenuJPanel extends JPanel{
         setSize(100,100);
         setLayout(new GridLayout(2,2));
 
+        //您的分数
         lblScroe1 = new JLabel();
         lblScroe1.setFont(new Font("微软雅黑", Font.PLAIN, 20));
         lblScroe1.setForeground(Color.BLACK);
         lblScroe1.setText("Your Score: "+score1);
         add(lblScroe1);
 
+        //对手的分数
         lblScroe2 = new JLabel();
         lblScroe2.setFont(new Font("微软雅黑", Font.PLAIN, 20));
         lblScroe2.setForeground(Color.BLACK);
         lblScroe2.setText("His Score: "+score2);
         add(lblScroe2);
 
+        //游戏状态信息
         lblInfo = new JLabel();
         lblInfo.setFont(new Font("微软雅黑", Font.PLAIN, 14));
         lblInfo.setForeground(Color.BLACK);
@@ -38,6 +41,11 @@ public class MenuJPanel extends JPanel{
         add(lblInfo);
     }
 
+    /**
+     *设置分数
+     * @param score1
+     * @param score2
+     */
     public void setScore(int score1,int score2){
         this.score1=score1;
         this.score2=score2;
@@ -46,6 +54,10 @@ public class MenuJPanel extends JPanel{
         lblScroe2.setText("His Score: "+score2);
     }
 
+    /**
+     * 设置状态信息
+     * @param s
+     */
     public void setInfo(String s){
         lblInfo.setText(s);
     }
