8198452c57029c4b5bde757c9d39e6855a5f2f05
zas023 <375027533@qq.com>
2018-07-31 23:12:24 +0800

    Create Folder PAT

---
+++ pat/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;  //times
+
+	cin>> n;  //Getting times
+
+	//Loop:
+	for (int i = 0; i < n; ++i)
+	{
+		cout<<"Hello C"<<endl;
+	}
+	return 0;
+}
